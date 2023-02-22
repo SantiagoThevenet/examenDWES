b3f49b1ec6657a351f9e7e48d06f94e134e97913
santiago <santithevenetvalles@gmail.com>
2023-02-22 11:00:28 +0100

    Creacion y estilos pagina inicio con bases de datos

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,4 @@
+DROP DATABASE fotosDB IF EXISTS;
 CREATE DATABASE fotosDB;
 USE fotosDB;
 
@@ -5,6 +6,7 @@ CREATE TABLE fotos(
     id INT PRIMARY KEY AUTO_INCREMENT,
     url VARCHAR(500),
     titulo VARCHAR(30),
+    descripcion VARCHAR(500),
     fecha timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     likes int,
     dislikes int
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require("mysql2/promise")
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+const { pool } = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "fotosDB"
+})
+
+router.get('/', async function (req, res, next) {
+  const [result] = await pool.promise().query("SELECT * FROM fotos")
+  console.log(result);
+  res.render('index', {
+    fotos: result
+  });
 });
 
-router.get('/masvotadas', function(req, res, next) {
+router.get('/create', function (req, res, next) {
+  res.render('crear');
+});
+router.post('/create', async function (req, res, next) {
+  const { url, titulo, descripcion } = req.body
+  const crearForm = {
+    url,
+    titulo,
+    descripcion ,
+    likes: 0,
+    dislikes: 0
+  }
+  console.log(crearForm);
+  const result = await pool.promise().query("INSERT INTO fotos SET ?", [crearForm])
+  console.log(result);
+  res.redirect('/fotos')
+});
+
+router.get('/edit', function (req, res, next) {
+  res.render('editar');
+});
+
+router.get('/masvotadas', function (req, res, next) {
   res.send("masvotadas")
 });
 
-router.get('/menosvotadas', function(req, res, next) {
+router.get('/menosvotadas', function (req, res, next) {
   res.send("menosvotadas")
 });
 
